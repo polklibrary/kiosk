f91e390e5fd1e27b57d3399a434d98511999be97
hietpasd <hietpasd@uwosh.edu>
2015-08-24 13:22:09 -0500

    modified startup

--- browser_startup_script.bat
+++ browser_startup_script.bat
@@ -1,3 +1,7 @@
-timeout 5
+timeout 10
+
+"C:\cygwin64\bin\bash.exe" "C:\svn\github\kiosk\trunk\update.sh" 
+
+timeout 10
 "C:\Users\lab\Desktop\Kiosk Mode - Chrome.lnk"
 ::start /d "C:\Program Files (x86)\Mozilla Firefox" firefox.exe
